3e907c46e32a3a50efa2a0997d411cf9f0488ac9
Alexa <space.tienpna@gmail.com>
2022-03-03 17:09:38 +0700

    [commit] update tutorial

--- Assets/Scripts/DestroyOutOfBound.cs
+++ Assets/Scripts/DestroyOutOfBound.cs
@@ -7,6 +7,8 @@ public class DestroyOutOfBound : MonoBehaviour
     // Start is called before the first frame update
     private float topBound = 34.0f;
     private float bottomBound = -15.0f;
+    private float leftBound = -25.0f;
+    private float rightBound = 25.0f;
     void Start()
     {
         
@@ -17,5 +19,7 @@ public class DestroyOutOfBound : MonoBehaviour
     {
         if(transform.position.z > topBound) Destroy(gameObject);
         else if(transform.position.z < bottomBound) Destroy(gameObject);
+        else if(transform.position.x < leftBound) Destroy(gameObject);
+        else if(transform.position.x > rightBound) Destroy(gameObject);
     }
 }
--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -7,11 +7,15 @@ public class SpawnManager : MonoBehaviour
     // Start is called before the first frame update
     public GameObject[] animalPrefabs;
     private int spawnRangeX = 20;
-    private float invokeTime = 1;
+    private int spawnRangeZ = 17;
+    private float invokeTime = 4;
+    private Quaternion rotation = new Quaternion();
     public float ratio = 1.5f;
     void Start()
     {
-        InvokeRepeating("SpawnAnimal", invokeTime, ratio);
+        InvokeRepeating("SpawnAnimalFromZ", invokeTime, ratio);
+        InvokeRepeating("SpawnAnimalFromX", invokeTime, ratio);
+        InvokeRepeating("SpawnAnimalFromXReverse", invokeTime, ratio);
     }
 
     // Update is called once per frame
@@ -19,11 +23,28 @@ public class SpawnManager : MonoBehaviour
     {
     }
 
-    void SpawnAnimal()
+    void SpawnAnimalFromZ()
     {
         int animalIndex = Random.Range(0, animalPrefabs.Length);
         int randomPosition = Random.Range(-spawnRangeX, spawnRangeX);
         Instantiate(animalPrefabs[animalIndex], new Vector3(randomPosition, 0, 30),
          animalPrefabs[animalIndex].transform.rotation);
+    }
+
+    void SpawnAnimalFromX()
+    {
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        int randomPosition = Random.Range(2, spawnRangeZ);
+        var animalRotation = Quaternion.Euler(0,90,0);
+        Instantiate(animalPrefabs[animalIndex], new Vector3(-25, 0, randomPosition),
+         animalRotation);
+    }
+    void SpawnAnimalFromXReverse()
+    {
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        int randomPosition = Random.Range(2, spawnRangeZ);
+        var animalRotation = Quaternion.Euler(0,-90,0);
+        Instantiate(animalPrefabs[animalIndex], new Vector3(25, 0, randomPosition),
+         animalRotation);
     }
 }
